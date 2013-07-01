fbd35c683769f5652aff40da1fd25f80a8a791bf
Gene Le <starfinzel@lark-book-pro.att.net>
2013-07-01 09:47:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeneBlog::Application.config.secret_token = 'd04433c35547beb516cacff3c8ca9095bec11cd5c17ae7f626125c417aee4d5adb54654895fd0972caf96a0881a9f09239c6bc60c366dee88df16058b35f536a'
